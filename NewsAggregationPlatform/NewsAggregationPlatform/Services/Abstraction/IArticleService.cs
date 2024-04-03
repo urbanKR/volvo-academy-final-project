57e52346339168333f4e5abb47ffa571ddb9d86a
Krzysztof Urban <urbankrzysz36@gmail.com>
2024-04-03 02:10:01 +0200

    Add Article Service.

---
+++ NewsAggregationPlatform/NewsAggregationPlatform/Services/Abstraction/IArticleService.cs
@@ -0,0 +1,14 @@
+﻿using NewsAggregationPlatform.Models.Entities;
+
+namespace NewsAggregationPlatform.Services.Abstraction
+{
+    public interface IArticleService
+    {
+        Task<IEnumerable<Article>> GetArticlesAsync();
+        Task<Article> GetArticleByIdAsync(Guid id);
+        bool AddArticle(Article article);
+        bool UpdateArticle(Article article);
+        bool DeleteArticle(Article article);
+        bool Save();
+    }
+}
---
+++ NewsAggregationPlatform/NewsAggregationPlatform/Services/Implementation/ArticleService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsAggregationPlatform.Data;
+using NewsAggregationPlatform.Models.Entities;
+using NewsAggregationPlatform.Services.Abstraction;
+
+namespace NewsAggregationPlatform.Services.Implementation
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ArticleService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<Article>> GetArticlesAsync()
+        {
+            return await _dbContext.Articles.ToListAsync();
+        }
+        public async Task<Article> GetArticleByIdAsync(Guid id)
+        {
+            return await _dbContext.Articles.FirstOrDefaultAsync(a => a.Id == id);
+        }
+        public bool AddArticle(Article article)
+        {
+            _dbContext.Articles.Add(article);
+            return Save();
+        }
+        public bool UpdateArticle(Article article)
+        {
+            _dbContext.Articles.Update(article);
+            return Save();
+        }
+        public bool DeleteArticle(Article article)
+        {
+            _dbContext.Remove(article);
+            return Save();
+        }
+        public bool Save()
+        {
+           var changes = _dbContext.SaveChanges();
+           return changes > 0;
+        }
+    }
+}
